2645ae1a0d1a6dfe78f70ddfe356001fb8be7173
S. Chris Colbert <sccolbert@gmail.com>
2015-09-03 15:53:13 -0400

    update to TS 1.6-beta

--- scripts/dtsbundle.js
+++
@@ -1,7 +0,0 @@
-require('dts-generator').generate({
-  name: 'phosphor-arrays',
-  main: 'phosphor-arrays/index',
-  baseDir: 'lib',
-  files: ['index.d.ts'],
-  out: 'lib/phosphor-arrays.d.ts',
-});
